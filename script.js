ee375a66c3b41845fb87f1c1d7fdda39ab08109f
ravexz444 <67143324+ravexz444@users.noreply.github.com>
2025-08-24 16:04:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -48,4 +48,78 @@ function createCollectionCheckboxes() {
 		checkbox.value = code;
 		checkbox.addEventListener("change", updateSkills);
 
-		const label
+		const label = document.createElement("label");
+		label.htmlFor = "collection-" + code;
+		label.textContent = `${code} – ${fullName}`;
+
+		div.appendChild(checkbox);
+		div.appendChild(label);
+		container.appendChild(div);
+	}
+}
+
+function updateSkills() {
+	const outputDiv = document.getElementById("skills-output");
+	outputDiv.innerHTML = "";
+
+	let chosenItems = [];
+
+	// Equipment + Retainers
+	equipmentTypes.forEach(type => {
+		if (type === "Accessory" || type === "Retainer") {
+			for (let i = 1; i <= 3; i++) {
+				const select = document.getElementById(`select-${type}-${i}`);
+				if (select && select.value) chosenItems.push(select.value);
+			}
+		} else {
+			const select = document.getElementById(`select-${type}`);
+			if (select && select.value) chosenItems.push(select.value);
+		}
+	});
+
+	// Collections (use codes, map to full names)
+	for (const code of Object.keys(collectionCodes)) {
+		const checkbox = document.getElementById("collection-" + code);
+		if (checkbox && checkbox.checked) {
+			const fullName = collectionCodes[code];
+			chosenItems.push(fullName);
+		}
+	}
+
+	// Display skills
+	chosenItems.forEach(itemName => {
+		let info = equipmentData[itemName] || collectionData[itemName];
+		if (info && info.skill) {
+			const itemDiv = document.createElement("div");
+			itemDiv.innerHTML = `<b>${itemName}</b><br>` +
+				info.skill.map(s => `- ${s[0]} (${s[1]})`).join("<br>");
+			outputDiv.appendChild(itemDiv);
+			outputDiv.appendChild(document.createElement("br"));
+		}
+	});
+}
+
+async function loadAllData() {
+	const [equip, coll, codes] = await Promise.all([
+		fetch("equipment_list.json").then(r => r.json()),
+		fetch("collection_list.json").then(r => r.json()),
+		fetch("collection_codes.json").then(r => r.json())
+	]);
+
+	equipmentData = equip;
+	collectionData = coll;
+	collectionCodes = codes;
+
+	// Build UI
+	equipmentTypes.forEach(type => {
+		if (type === "Accessory" || type === "Retainer") {
+			for (let i = 1; i <= 3; i++) createDropdown(type, i);
+		} else {
+			createDropdown(type);
+		}
+	});
+	createCollectionCheckboxes();
+}
+
+// run on page load
+loadAllData();
